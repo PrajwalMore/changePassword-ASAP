09da3e143b7d21cd794d9d4b91c98aa8075d6425
CodeStorm <31198108+PrajwalMore@users.noreply.github.com>
2021-05-25 20:39:44 +0530

    Add files via upload

---
+++ openBrowser.py
@@ -0,0 +1,22 @@
+import webbrowser
+
+##url = 'https://pythonexamples.org'
+print("Application launched")
+webbrowser.register('chrome',
+	None,
+	webbrowser.BackgroundBrowser("C:\Program Files\Google\Chrome\Application/chrome.exe"))
+
+setOfUrl={"google":"google password change url here"}
+
+
+def addLink(urlName,url):
+    setOfUrl[urlName]=url
+addLink("github","github password change url here.")
+#addLink("onlineSite","password change service url")
+#addLink("","")
+#addLink("","")
+#addLink("","")
+# ^ multiple links to open.
+
+for url in setOfUrl:
+          webbrowser.get('chrome').open_new(setOfUrl[url])
